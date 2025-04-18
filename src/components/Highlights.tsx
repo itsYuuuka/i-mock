ec96674324f81e767fe7fbdeaa847475b667c8a4
itsYuuuka <okan_altun@hotmail.com>
2025-04-18 03:34:47 +0200

    Add GSAP animation to Highlights component for fade-in effect

--- src/components/Highlights.tsx
+++ src/components/Highlights.tsx
@@ -1,9 +1,25 @@
-const Highlights = () => {
-  return (
-    <section id="highlights">
-        
-    </section>
-  )
-}
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
-export default Highlights
+const Highlights = () => {
+  useGSAP(() => {
+    gsap.to("#title", { opacity: 1, y: 0 });
+  }, []);
+
+  return (
+    <section
+      id="highlights"
+      className="w-screen overflow-hidden h-full common-padding bg-zinc"
+    >
+      <div className="screen-max-widht">
+        <div>
+          <h1 id="title" className="section-heading">
+            Get the highlights
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Highlights;
