5f4eb0ff407623b26e314a99ae037cf08a8f63c7
itsYuuuka <okan_altun@hotmail.com>
2025-04-18 20:49:26 +0200

    Refactor Model component to include GSAP animations and improve structure

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,9 +1,23 @@
-const Model = () => {
-  return (
-    <section>
-        
-    </section>
-  )
-}
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
-export default Model
+const Model = () => {
+  useGSAP(() => {
+    gsap.to("#heading", {
+      y: 0,
+      opacity: 1,
+    });
+  }, []);
+
+  return (
+    <section className="common-padding">
+      <div className="screen-max-width">
+        <h1 id="heading" className="section-heading">
+          Take a closer look.
+        </h1>
+      </div>
+    </section>
+  );
+};
+
+export default Model;
