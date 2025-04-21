b3563333363a6e992f5a2f42abd6fee3370a3951
itsYuuuka <okan_altun@hotmail.com>
2025-04-21 23:37:03 +0200

    Add size selection functionality to Model component

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -6,7 +6,7 @@ import { yellowImg } from "../utils";
 import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
-import { models } from "../constants/index";
+import { models, sizes } from "../constants/index";
 
 const Model = () => {
   const [size, setSize] = useState("small");
@@ -88,6 +88,21 @@ const Model = () => {
                   />
                 ))}
               </ul>
+              <button className="size-btn-container">
+                {sizes.map(({ label, value }) => (
+                  <span
+                    key={label}
+                    className="size-btn"
+                    style={{
+                      backgroundColor: size === value ? "white" : "transparent",
+                      color: size === value ? "black" : "white",
+                    }}
+                    onClick={() => setSize(value)}
+                  >
+                    {label}
+                  </span>
+                ))}
+              </button>
             </div>
           </div>
         </div>
