686d6f27bcbf22ffbb18b53f8fe47d59420ee3e8
itsYuuuka <okan_altun@hotmail.com>
2025-04-22 00:00:33 +0200

    Add Model component for rendering iPhone in 3D scene

--- src/components/ModelView.tsx
+++ src/components/ModelView.tsx
@@ -1,7 +1,9 @@
 import { Group } from "three";
-import { RefObject, Dispatch, SetStateAction } from "react";
+import { RefObject, Dispatch, SetStateAction, Suspense } from "react";
 import { PerspectiveCamera, View } from "@react-three/drei";
 import Lights from "./Lights";
+import Model from "./iPhone";
+import { Html } from "@react-three/drei";
 
 interface ModelViewProps {
   index: number;
@@ -38,6 +40,16 @@ const ModelView = ({
       <PerspectiveCamera makeDefault position={[0, 0, 4]} />
 
       <Lights />
+
+      <Suspense
+  fallback={
+    <Html>
+      <div>Loading...</div>
+    </Html>
+  }
+>
+  <Model />
+</Suspense>
     </View>
   );
 };
