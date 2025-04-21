b8d671bf7f1ab3fe6804fde1ef0928313bacd0ff
itsYuuuka <okan_altun@hotmail.com>
2025-04-21 23:49:58 +0200

    Add Lights component to enhance lighting in the 3D scene

--- src/components/ModelView.tsx
+++ src/components/ModelView.tsx
@@ -1,5 +1,7 @@
 import { Group } from "three";
 import { RefObject, Dispatch, SetStateAction } from "react";
+import { PerspectiveCamera, View } from "@react-three/drei";
+import Lights from "./Lights";
 
 interface ModelViewProps {
   index: number;
@@ -25,9 +27,18 @@ const ModelView = ({
   size,
 }: ModelViewProps) => {
   return (
-    <div>
-      
-    </div>
+    <View
+      index={index}
+      id={gsapType}
+      className={`border-2 border-red-500 w-full h-full
+        ${index === 2} ? 'right-[-100%] : '' `}
+    >
+      <ambientLight intensity={0.3} />
+
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+      <Lights />
+    </View>
   );
 };
 
