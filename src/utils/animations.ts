1370e8a9271bab8415e2dcc4e45733bd732298d1
itsYuuuka <okan_altun@hotmail.com>
2025-04-23 00:04:49 +0200

    Refactor animateWithGsapTimeline function to accept parameters for enhanced flexibility and improved animation handling

--- src/utils/animations.ts
+++ src/utils/animations.ts
@@ -1,3 +1,35 @@
-export const animateWithGsapTimeline = () => {
+import { RefObject } from "react";
+import * as THREE from "three";
+
+export const animateWithGsapTimeline = (
+  timeline: gsap.core.Timeline,
+  rotationRef: RefObject<THREE.Group>,
+  rotationState: RefObject<THREE.Group>,
+  firstTarget: string,
+  secondTarget: string,
+  animationProps: gsap.TweenVars
+) => {
+    timeline.to(rotationRef.current.rotation, {
+        y: rotationState.current.rotation.y,
+        duration: 1,
+        ease: "power2.inOut",
+    })
+
+    timeline.to(
+        firstTarget,
+        {
+            ...animationProps,
+            ease: "power2.inOut",
+        },
+        "<"
+    )
     
-}
+    timeline.to(
+        secondTarget,
+        {
+            ...animationProps,
+            ease: "power2.inOut",
+        },
+        "<"
+    )
+};
